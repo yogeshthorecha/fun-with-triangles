87d1a68e9ea05b9c9e99cd9c37e8136b56a1f860
yogeshthorecha <118168243+yogeshthorecha@users.noreply.github.com>
2022-12-25 10:08:39 +0530

    Update istriangle.js

--- istriangle.js
+++ istriangle.js
@@ -15,9 +15,14 @@ function calculateSumOfAngles() {
 
 
 isTriangleBtn.addEventListener('click',function(){
+    if(inputs[0].value>=0 && inputs[1].value>=0 && inputs[2].value>=0){
     var sum = calculateSumOfAngles();
     if(sum === 180){
         outputBox.innerText = "yes! this angles make a triangle";
     }
     else outputBox.innerText = "No! this angles cannot make a triangle";
+    }
+    else{
+        outputBox.innerText = "please enter positive values";
+    }
 })
